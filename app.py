1ca9f952a4fed5ae13bd56f9e298345b1a3d9a1e
hoangvuong <hoangvm92@gmail.com>
2022-03-20 22:17:24 +0700

    test

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, FU!!!'
+    return 'Please subscribe, like, and comment on this video, FU this much!!!'
